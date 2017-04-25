9352af087ba8db71e8cf83848ca72ee9c142a28c
miokato@me.com <mio_k@mm.local>
2017-04-25 14:13:42 +0900

    add function to save data that coming from linebot.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import json
+from datetime import datetime
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -26,6 +29,8 @@ def callback():
 
     body = request.get_data(as_text=True)
     app.logger.info('Request body: ' + body)
+    # データをファイルとして保存
+    save_json(body)
 
     # handle webhook body
     try:
@@ -38,10 +43,28 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    id = event
+    #profile = line_bot_api.get_profile(id)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)
     )
+
+
+def save_json(dic_or_json):
+    """json形式か辞書型のデータを整形してjsonファイルとして書き出す"""
+    dir = './data/'
+    now = datetime.now().strftime('%Y%m%d_%H%M_%s')
+    prefix = 'time-'
+    suffix = '.json'
+    file = dir + prefix + now + suffix
+    if isinstance(dic_or_json, dict):
+        dic = dic_or_json
+    else:
+        dic = json.loads(dic_or_json)
+
+    with open(file, 'wt') as f:
+        json.dump(dic, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
