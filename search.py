862b79f23322d4fe14d31ac8732e2345756ccc56
miokato <miokato@me.com>
2017-05-11 20:42:33 +0900

    add BookScraper class

--- search.py
+++ search.py
@@ -7,6 +7,59 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
+class BookScraper(object):
+    search_url = 'http://www.honyaclub.com/shop/goods/search.aspx?'
+    main_url = 'https://www.honyaclub.com'
+    keyword = ''
+    number_of_books = 1
+    books_title = []
+    books_image_url = []
+    books_detail_url = []
+
+    def fetch(self, keyword, number_of_books=1):
+        self.keyword = keyword
+        self.number_of_books = number_of_books
+
+        encode_word = self.keyword.encode('Shift-JIS')
+
+        payload = {
+            'search': 'x',
+            'keyw': encode_word,
+        }
+
+        if self.number_of_books <= 0 or self.number_of_books > 5:
+            return None
+
+        r = requests.get(self.search_url, params=payload)
+        soup = bs(r.content, 'html.parser')
+        try:
+            books_data = soup.find('div', class_='result-item')
+            # それぞれの本を取得
+            books_detail = books_data.find_all('div', class_='item-img', limit=self.number_of_books)
+            # get alt elements
+            self.books_title = [book.img.get('alt') for book in books_detail]
+            # get src elements
+            books_image_elements = [book.img.get('src') for book in books_detail]
+            self.books_image_url = [urljoin(self.main_url, path) for path in books_image_elements]
+            # get href elements
+            books_detail_elements = [book.a.get('href') for book in books_detail]
+            self.books_detail_url = [urljoin(self.main_url, path) for path in books_detail_elements]
+
+            # get author data and url
+            books_info = books_data.find_all('dl', class_='item-txt')
+            books_author = [book.find('dd').find('a') for book in books_info]
+
+        except AttributeError:
+            return None
+
+        return True
+
+    def dump(self):
+        return (self.books_title,
+                self.books_image_url,
+                self.books_detail_url)
+
+
 def fetch_books(keyword, number_of_books=1):
     """
     Honya Clubから本のタイトル、詳細リンク、画像リンクを取得する
@@ -52,8 +105,10 @@ def fetch_books(keyword, number_of_books=1):
 
 
 if __name__ == '__main__':
+    scraper = BookScraper('ねこ', number_of_books=2)
     try:
-        books_title, books_image_url, books_detail_url = fetch_books('ねこ', number_of_books=3)
+        scraper.fetch()
     except TypeError:
         print('本はありません')
-    print(books_detail_url)
+
+    print(scraper.dump())
