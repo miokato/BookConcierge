da3bb1306e9ba4e13714f5d69225f2666c88d55c
miokato <miokato@me.com>
2017-05-11 20:43:23 +0900

    add funciton that check input message

--- parse.py
+++ parse.py
@@ -1,15 +1,20 @@
-class TextParser(object):
-    def __init__(self):
-        pass
-
-    def set_text(self, text):
-        self.text = text
-        return self.text
-
-    def parse_str_to_word(self, str=''):
-        str_list = [word for word in str]
-        return str_list
+"""
+Parse input data from line app
+"""
 
 
-if __name__ == '__main__':
-    pass
+long_sample_message = 'あの花の名前をまだ僕は知らないを見た人の感想を知りたい'
+short_sample_message = 'あの花'
+blank_sample_message = ''
+
+
+def cutoff_msg20(input):
+    try:
+        msg_length = len(input)
+    except TypeError:
+        return ''
+    if msg_length > 20:
+        return '20文字以内の単語でしらべてほしいにゃー'
+
+    return input
+
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,37 +1,30 @@
 import unittest
 from nose.tools import ok_, eq_
-from parse import TextParser
+
+from parse import cutoff_msg20
 
 
-class TestStringMethods(unittest.TestCase):
-    """テストケースサンプル"""
-    def test_upper(self):
-        eq_('foo'.upper(), 'FOO')
-        self.assertEqual('foo'.upper(), 'FOO')
+class TestCutOffMsg20(unittest.TestCase):
+    """
+    LINEから入力された文字列が渡されるので、入力はすべてstr
+    """
+    def test_cutoff_msg20_with_japanese_msg(self):
+        msg = 'あの花'
+        eq_(cutoff_msg20(msg), msg)
 
-    def test_isupper(self):
-        ok_('FOO'.isupper())
-        ok_(not 'Foo'.isupper())
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_cutoff_msg20_with_english_msg(self):
+        msg = 'anohana'
+        eq_(cutoff_msg20(msg), msg)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_cutoff_msg20_with_long_msg(self):
+        msg = 'あの花の名前を僕はまだ知らないを見たんだけど、君どう思う?'
+        eq_(cutoff_msg20(msg), '20文字以内の単語でしらべてほしいにゃー')
 
+    def test_cutoff_msg20_with_blank_msg(self):
+        msg = ''
+        eq_(cutoff_msg20(msg), '')
 
-class TestTextParser(unittest.TestCase):
-    def setUp(self):
-        self.parser = TextParser()
+    def test_cutoff_msg20_with_none(self):
+        msg = None
+        eq_(cutoff_msg20(msg), '')
 
-    def test_set_text(self):
-        # 第三引数にテストが失敗した時のメッセージを出すことができる
-        self.assertEqual(self.parser.set_text('hello'), 'hello', 'incorrect')
-
-    def test_parse_str_to_word(self):
-        self.assertEqual(self.parser.parse_str_to_word('hello'), ['h','e','l','l','o'])
-
-    def test_is_list_parse_str_to_word(self):
-        self.assertTrue(isinstance(self.parser.parse_str_to_word(), list))
